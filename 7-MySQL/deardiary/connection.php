7106b053986e022730afc7c6ef796d9d8cd5f36d
Ansh Rastogi <60509184+anshrastogi240901@users.noreply.github.com>
2020-07-21 16:54:24 +0530

    Update connection.php

--- 7-MySQL/deardiary/connection.php
+++ 7-MySQL/deardiary/connection.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect("shareddb-u.hosting.stackcp.net","UserCredentials-3133339199","thehacker007@","UserCredentials-3133339199");
+    $link = mysqli_connect("yourhosturl","database name","database password","table name same as database name in my case");
         
         if (mysqli_connect_error()) {
             
