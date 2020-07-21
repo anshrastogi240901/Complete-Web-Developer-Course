3905f299420b2ee0accb41f1332e180ba8e2097d
Ansh Rastogi <60509184+anshrastogi240901@users.noreply.github.com>
2020-07-21 14:00:41 +0530

    Add files via upload

---
+++ css/index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta http-equiv="cache-control" content="max-age=0" />
+  <meta http-equiv="cache-control" content="no-cache" />
+  <meta http-equiv="expires" content="0" />
+  <meta http-equiv="pragma" content="no-cache" />
+  <title>This site is brand new</title>
+  <style>
+
+
+  html
+  {
+    font-size: 62.5%;
+  }
+
+  body
+  {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    color: #353535;
+    background: linear-gradient(to bottom, #ccc, #fff 30%);
+    background-repeat: no-repeat;
+  }
+
+  h1,h3
+  {
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    text-transform: uppercase;
+  }
+
+
+  h1
+  {
+    margin: 20rem 0 1rem;
+    font-size: 3rem;
+  }
+
+  h3
+  {
+    font-weight: normal;
+    font-size: 1.4rem;
+  }
+  </style>
+</head>
+<body>
+  <h1>This Site is Brand New</h1>
+  <h3>Please check back again soon</h3>
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</body>
+</html>
